9bec18077a986f6e1eb8400895b65d69b3ab4138
arianpopalyar <arian_popalyar@yahoo.com>
2019-05-14 16:24:04 +0100

    adding the taks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,53 +6,75 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useCreateIndex:true
 })
 
-const User = mongoose.model('Users',{
-    name:{
-        type:String,
-        required:true,
-        trim:true
+const Task = mongoose.model('Tasks',{
+    description:{
+        type: 'String',
+        require: true,
+        trim: true
     },
-    age:{
-        type:Number,
-        validate(value){
-        if(value < 0){
-          throw new Error('Age can not be less then 1')
-        }
-        }
-    },
-    email:{
-        type:String,
-        trim:true,
-        lowercase:true,
-        require:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Emai is invalid')
-            }
-        }
-    },
-    password:{
-        require:true,
-        type:String,
-        trim:true,
-        minLength:7,
-        validate(value){    
-          if(value.toLowerCase().includes('password')){
-                throw new Error('Password can not contain password')
-            }
-        }
+    completed:{
+        type: Boolean,
+        default: false
     }
 })
 
-const saveUser = new User({
-    name:'Joker',
-    age:0,
-    email:'arian@test.com',
-    password:'whatever334%' 
-})
+// const User = mongoose.model('Users',{
+//     name:{
+//         type:String,
+//         required:true,
+//         trim:true
+//     },
+//     age:{
+//         type:Number,
+//         validate(value){
+//         if(value < 0){
+//           throw new Error('Age can not be less then 1')
+//         }
+//         }
+//     },
+//     email:{
+//         type:String,
+//         trim:true,
+//         lowercase:true,
+//         require:true,
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 throw new Error('Emai is invalid')
+//             }
+//         }
+//     },
+//     password:{
+//         require:true,
+//         type:String,
+//         trim:true,
+//         minLength:7,
+//         validate(value){    
+//           if(value.toLowerCase().includes('password')){
+//                 throw new Error('Password can not contain password')
+//             }
+//         }
+//     }
+// })
 
-saveUser.save().then(()=>{
-    console.log(saveUser)
+// const saveUser = new User({
+//     name:'Joker',
+//     age:0,
+//     email:'arian@test.com',
+//     password:'whatever334%' 
+// })
+
+
+// saveUser.save().then(()=>{
+//     console.log(saveUser)
+// }).catch((error)=>{
+//     console.log(error)
+// })
+const saveTask = new Task({
+    description:'paint the doors',
+   // completed:false
+})
+saveTask.save().then(()=>{
+    console.log(saveTask)
 }).catch((error)=>{
     console.log(error)
 })
