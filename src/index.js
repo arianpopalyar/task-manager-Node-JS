582339810812ca27baa88e73a0cd89c1a3e6199f
arianpopalyar <arian_popalyar@yahoo.com>
2019-06-05 15:27:46 +0100

    automating authentication

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task.js')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-// app.use((req,res,next) => {
-//     if(req.method === 'GET'){
-//         res.send('GET requests are disabled ')
-//     }else{
-//         next()
-//     }
-  
-// })
-
-
-//code for under maintinance
-app.use((req, res, next) =>{
-    res.status(503).send('site is under maintinance')
-})
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
 app.listen(port, () => {
-    console.log('Server is up on port '+ port)
+    console.log('Server is up on port' + port)
 })
-
-//const bcrypt = require('bcryptjs')
-//esample code for
-const jwt = require('jsonwebtoken')
-const myFunction = async ()=>{
-   const token = jwt.sign({_id:'abc123'}, 'thisismycourse',{expiresIn:'7 days'})
-   console.log(token)
-
-   const data = jwt.verify(token, 'thisismycourse')
-   console.log(data)
-}
-myFunction()
--- src/models/user.js
+++ src/models/user.js
@@ -4,81 +4,98 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        trim:true
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    age:{
-        type:Number,
-        required:true,
-        validate(value){
-        if(value < 0){
-          throw new Error('Age can not be less then 1')
-        }
-        }
-    },
-    email:{
+    email: {
         type: String,
         unique: true,
+        required: true,
         trim: true,
         lowercase: true,
-        require: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Emai is invalid')
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
             }
         }
     },
-    password:{
-        require:true,
-        type:String,
-        trim:true,
-        minlength:7,
-        validate(value){    
-          if(value.toLowerCase().includes('password')){
-                throw new Error('Password can not contain password')
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
             }
         }
     },
-    tokens:[{
-        token:{
-            type:String,
-            required:true
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
         }
     }]
 })
-userSchema.methods.generateAuthToken = async function(){
+
+userSchema.methods.toJSON = function () {
     const user = this
-    const token = jwt.sign({_id:user._id.toString() }, 'thisismynewcourse')
-   
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
     user.tokens = user.tokens.concat({ token })
     await user.save()
+
     return token
 }
-userSchema.statics.findByCredentials = async (email, password) =>{
-    const user = await User.findOne({email})
 
-    if(!user){
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
         throw new Error('Unable to login')
     }
 
     const isMatch = await bcrypt.compare(password, user.password)
-    if(!isMatch){
-        throw new Error ('Unable to login')
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
     }
 
     return user
 }
-//Has the plain text password before saving
-userSchema.pre('save', async function(next){
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
     const user = this
-   
-    if(user.isModified('password')){
+
+    if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
+
     next()
 })
-const User = mongoose.model('Users', userSchema)
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
