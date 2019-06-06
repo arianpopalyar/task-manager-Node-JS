667969263ce49bf771a9a9a2a8ee046b27937fca
arianpopalyar <arian_popalyar@yahoo.com>
2019-06-06 16:22:30 +0100

    new code

--- src/models/task.js
+++ src/models/task.js
@@ -1,23 +1,20 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
 
-const taskSchema = new mongoose.Schema({
-        description:{
-            type: 'String',
-            require: true,
-            trim: true
-        },
-        completed:{
-            type: Boolean,
-            default: false
-        }
-    })
-taskSchema.pre('save', async function(next){
-    const task = this
-    
-    next()
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
 })
-const Task = mongoose.model('Tasks',taskSchema)
 
 module.exports = Task
