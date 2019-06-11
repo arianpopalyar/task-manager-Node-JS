8724f74c0f3e32dc3ebb02e94ecc2ea309131119
arianpopalyar <arian_popalyar@yahoo.com>
2019-06-11 16:29:25 +0100

    changes to the file upload limit

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -77,6 +79,13 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
+})
+const upload = multer({
+    dest:'avatar'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'),(req, res)=>{
+    res.send()
 })
 
 module.exports = router
